34e48d0f033e4c087ba341137655e05300c62b06
Kyrylo Sovailo <k.sovailo@gmail.com>
2023-05-05 14:36:45 +0200

    First templates written

--- include/betterdouble/betterdouble.h
+++ include/betterdouble/betterdouble.h
@@ -1,61 +1,65 @@
 #pragma once
 
-#include <math.h>
+#include <cmath>
 
 namespace bd
 {
     ///Same as double, but with overloaded operators
-    class Double
+    template<class T>
+    class Real
     {
     public:
-        double value;
-        inline Double() {};
-        inline Double(double value) : value(value) {};
-        inline Double(const Double &other) : value(other.value) {};
-        inline Double operator+  ()                    const noexcept { return value; };
-        inline Double operator-  ()                    const noexcept { return -value; };
-        inline Double &operator++()                    noexcept       { ++value; return *this; };
-        inline Double &operator--()                    noexcept       { --value; return *this; };
-        inline Double operator++ (int)                 noexcept       { double old = value; ++value; return old; };
-        inline Double operator-- (int)                 noexcept       { double old = value; --value; return old; };
-        inline Double &operator= (const Double &other) noexcept       { value = other.value;  return *this; };
-        inline Double &operator+=(const Double &other) noexcept       { value += other.value; return *this; };
-        inline Double &operator-=(const Double &other) noexcept       { value -= other.value; return *this; };
-        inline Double &operator*=(const Double &other) noexcept       { value *= other.value; return *this; };
-        inline Double &operator/=(const Double &other) noexcept       { value /= other.value; return *this; };
-        inline operator bool     ()                    const noexcept { return value; };
+        T value;
+        inline Real() {};
+        inline Real(T value) : value(value) {};
+        inline Real(const Real &other) : value(other.value) {};
+        inline Real operator+  ()                  const noexcept { return value; };
+        inline Real operator-  ()                  const noexcept { return -value; };
+        inline Real &operator++()                  noexcept       { ++value; return *this; };
+        inline Real &operator--()                  noexcept       { --value; return *this; };
+        inline Real operator++ (int)               noexcept       { T old = value; ++value; return old; };
+        inline Real operator-- (int)               noexcept       { T old = value; --value; return old; };
+        inline Real &operator= (const Real &other) noexcept       { value = other.value;  return *this; };
+        inline Real &operator+=(const Real &other) noexcept       { value += other.value; return *this; };
+        inline Real &operator-=(const Real &other) noexcept       { value -= other.value; return *this; };
+        inline Real &operator*=(const Real &other) noexcept       { value *= other.value; return *this; };
+        inline Real &operator/=(const Real &other) noexcept       { value /= other.value; return *this; };
+        inline operator bool   ()                  const noexcept { return value; };
     };
 
-    inline Double operator+ (const Double &a, const Double &b) { return a.value +  b.value; };
-    inline Double operator- (const Double &a, const Double &b) { return a.value -  b.value; };
-    inline Double operator* (const Double &a, const Double &b) { return a.value *  b.value; };
-    inline Double operator/ (const Double &a, const Double &b) { return a.value /  b.value; };
-    inline Double operator==(const Double &a, const Double &b) { return a.value == b.value; };
-    inline Double operator!=(const Double &a, const Double &b) { return a.value != b.value; };
-    inline Double operator> (const Double &a, const Double &b) { return a.value >  b.value; };
-    inline Double operator< (const Double &a, const Double &b) { return a.value <  b.value; };
-    inline Double operator>=(const Double &a, const Double &b) { return a.value >= b.value; };
-    inline Double operator<=(const Double &a, const Double &b) { return a.value <= b.value; };
+    template<class T> inline Real<T> operator+ (const Real<T> &a, const Real<T> &b) { return a.value +  b.value; };
+    template<class T> inline Real<T> operator- (const Real<T> &a, const Real<T> &b) { return a.value -  b.value; };
+    template<class T> inline Real<T> operator* (const Real<T> &a, const Real<T> &b) { return a.value *  b.value; };
+    template<class T> inline Real<T> operator/ (const Real<T> &a, const Real<T> &b) { return a.value /  b.value; };
+    template<class T> inline Real<T> operator==(const Real<T> &a, const Real<T> &b) { return a.value == b.value; };
+    template<class T> inline Real<T> operator!=(const Real<T> &a, const Real<T> &b) { return a.value != b.value; };
+    template<class T> inline Real<T> operator> (const Real<T> &a, const Real<T> &b) { return a.value >  b.value; };
+    template<class T> inline Real<T> operator< (const Real<T> &a, const Real<T> &b) { return a.value <  b.value; };
+    template<class T> inline Real<T> operator>=(const Real<T> &a, const Real<T> &b) { return a.value >= b.value; };
+    template<class T> inline Real<T> operator<=(const Real<T> &a, const Real<T> &b) { return a.value <= b.value; };
 
-    inline Double acos (const Double &a) noexcept { return ::acos (a.value); };
-    inline Double asin (const Double &a) noexcept { return ::asin (a.value); };
-    inline Double atan (const Double &a) noexcept { return ::atan (a.value); };
-    inline Double cos  (const Double &a) noexcept { return ::cos  (a.value); };
-    inline Double cosh (const Double &a) noexcept { return ::cosh (a.value); };
-    inline Double sin  (const Double &a) noexcept { return ::sin  (a.value); };
-    inline Double sinh (const Double &a) noexcept { return ::sinh (a.value); };
-    inline Double tanh (const Double &a) noexcept { return ::tanh (a.value); };
-    inline Double exp  (const Double &a) noexcept { return ::exp  (a.value); };
-    inline Double log  (const Double &a) noexcept { return ::log  (a.value); };
-    inline Double log10(const Double &a) noexcept { return ::log10(a.value); };
-    inline Double sqrt (const Double &a) noexcept { return ::sqrt (a.value); };
-    inline Double ceil (const Double &a) noexcept { return ::ceil (a.value); };
-    inline Double fabs (const Double &a) noexcept { return ::fabs (a.value); };
-    inline Double floor(const Double &a) noexcept { return ::floor(a.value); };
-    inline Double atan2(const Double &a, const Double &b) noexcept { return ::atan2(a.value, b.value);   };
-    inline Double pow  (const Double &a, const Double &b) noexcept { return ::pow  (a.value, b.value);   };
-    inline Double fmod (const Double &a, const Double &b) noexcept { return ::fmod (a.value, b.value);   };
-    inline Double frexp(const Double &a, int *b)          noexcept { return ::frexp(a.value, b);         };
-    inline Double ldexp(const Double &a, int b)           noexcept { return ::ldexp(a.value, b);         };
-    inline Double modf (const Double &a, Double *b)       noexcept { return ::modf (a.value, &b->value); };
+    template<class T> inline Real<T> acos (const Real<T> &a) noexcept { return std::acos (a.value); };
+    template<class T> inline Real<T> asin (const Real<T> &a) noexcept { return std::asin (a.value); };
+    template<class T> inline Real<T> atan (const Real<T> &a) noexcept { return std::atan (a.value); };
+    template<class T> inline Real<T> cos  (const Real<T> &a) noexcept { return std::cos  (a.value); };
+    template<class T> inline Real<T> cosh (const Real<T> &a) noexcept { return std::cosh (a.value); };
+    template<class T> inline Real<T> sin  (const Real<T> &a) noexcept { return std::sin  (a.value); };
+    template<class T> inline Real<T> sinh (const Real<T> &a) noexcept { return std::sinh (a.value); };
+    template<class T> inline Real<T> tanh (const Real<T> &a) noexcept { return std::tanh (a.value); };
+    template<class T> inline Real<T> exp  (const Real<T> &a) noexcept { return std::exp  (a.value); };
+    template<class T> inline Real<T> log  (const Real<T> &a) noexcept { return std::log  (a.value); };
+    template<class T> inline Real<T> log10(const Real<T> &a) noexcept { return std::log10(a.value); };
+    template<class T> inline Real<T> sqrt (const Real<T> &a) noexcept { return std::sqrt (a.value); };
+    template<class T> inline Real<T> ceil (const Real<T> &a) noexcept { return std::ceil (a.value); };
+    template<class T> inline Real<T> fabs (const Real<T> &a) noexcept { return std::fabs (a.value); };
+    template<class T> inline Real<T> floor(const Real<T> &a) noexcept { return std::floor(a.value); };
+    template<class T> inline Real<T> atan2(const Real<T> &a, const Real<T> &b) noexcept { return std::atan2(a.value, b.value);   };
+    template<class T> inline Real<T> pow  (const Real<T> &a, const Real<T> &b) noexcept { return std::pow  (a.value, b.value);   };
+    template<class T> inline Real<T> fmod (const Real<T> &a, const Real<T> &b) noexcept { return std::fmod (a.value, b.value);   };
+    template<class T> inline Real<T> frexp(const Real<T> &a, int *b)           noexcept { return std::frexp(a.value, b);         };
+    template<class T> inline Real<T> ldexp(const Real<T> &a, int b)            noexcept { return std::ldexp(a.value, b);         };
+    template<class T> inline Real<T> modf (const Real<T> &a, Real<T> *b)       noexcept { return std::modf (a.value, &b->value); };
+
+    typedef Real<double> Double;
+    typedef Real<float> Float;
 }
